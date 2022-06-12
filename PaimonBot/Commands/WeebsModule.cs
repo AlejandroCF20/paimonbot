82d643025553808ba6d61e9e9f60e365cd1cf0b6
Alejandro Cadena <alejandrocf.dev@gmail.com>
2022-06-12 10:15:47 -0500

    first commit

---
+++ PaimonBot/Commands/WeebsModule.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace PaimonBot.Commands;
+
+public class WeebsModule : BaseCommandModule
+{
+    [Command("anime")]
+    public async Task AnimeCommand(CommandContext ctx, string source, string title)
+    {
+        ctx.Message.Attachments
+    }
+}
---
+++ PaimonBot/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using PaimonBot.Commands;
+
+Console.WriteLine("Starting bot process...");
+MainAsync().GetAwaiter().GetResult();
+
+static async Task MainAsync()
+{
+    var discord = new DiscordClient(new DiscordConfiguration()
+    {
+        Token = "OTgyNzg5ODI0MjQ1Mjg4OTcw.GzuS-8.U8CB-rE7QtDFApVim7SVnpx6h63nDdkpRdKXIs",
+        TokenType = TokenType.Bot
+    });
+    
+    var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+    {
+        StringPrefixes = new[] { "!" }
+    });
+    
+    commands.RegisterCommands<WeebsModule>();
+    
+    await discord.ConnectAsync();
+    await Task.Delay(-1);
+}
